b3c73e068a0c7bf5dbb0163b0e3d5017086f8da2
marizoo <marizoo@meow.woof>
2021-12-18 20:17:19 +0700

    just before starting json server

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-a WHAT website created with React + styled component
-+ using WHAT...
+a simple blog created with React + styled component
++ using useState and useFetch.
++ practise fetching data with JSON server.
 ![Screenshot of the site](./screenshots/Agensy-website-800.png)
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,8 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
+    const [name, setName] = useState('mario')
+
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -16,12 +18,14 @@ const Home = () => {
 
     useEffect( () => {
         console.log('use effect ran');
-        console.log(blogs);
-    } )
+        console.log(name);
+    }, [name] )
 
     return (
         <div>
             <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
+            <button onClick={() => setName('luigi')}>change Name</button>
+            {name}
         </div>
     )
 }
