ed8d7b78692b50a44bf2df335fb08d78ef56c5de
marizoo <marizoo@meow.woof>
2021-12-20 13:52:21 +0700

    added add new blog form

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,15 +1,46 @@
 import {useParams} from 'react-router-dom'
 import styled from "styled-components";
+import useFetch from './useFetch';
 
 
-const Container = styled.div``;
+const Container = styled.div`
+
+h2 {
+  font-size: 20px;
+  color: #f1356d;
+  margin-bottom: 10px;
+}
+
+div{
+  margin: 20px 0;
+}
+
+/* button{
+  background: #f1356d;
+  color: #fff;
+  border: 0;
+  padding: 8px;
+  border-radius: 8px;
+  cursor: pointer;
+} */
+`;
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id);
+
 
   return (
     <Container>
-      <h2>Blog Details - { id }</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </Container>
   );
 };
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,15 +1,77 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components';
 
 const Container = styled.div`
+max-width: 400px;
+margin 0 auto;
+text-align: center;
 
+label {
+    text-align: left;
+    display: block;
+}
+
+h2 {
+    font-size: 20px;
+    color: #f1356d;
+    margin-bottom: 30px;
+}
+
+input, textarea, select {
+    width: 100%;
+    padding: 6px 10px;
+    margin: 10px 0;
+    border: 1px solid #ddd;
+    box-sizing: border-box;
+    display: block;
+}
+
+button {
+    background: #f1356d;
+    color: #fff;
+    border: 0;
+    padding: 8px;
+    border-radius: 8px;
+    cursor: pointer;
+}
 `;
 
 
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
     return (
         <Container>
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                type="text" 
+                required
+                value={title}
+                onChange={(ev) => setTitle(ev.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                required
+                value={body}
+                onChange={(ev) => setBody(ev.target.value)}
+                >
+                </textarea>
+                <label>Blog author:</label>
+                <select
+                value={author}
+                onChange={(ev) => setAuthor(ev.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                
+            </form>
         </Container>
     )
 }
